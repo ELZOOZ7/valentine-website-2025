9fb0412acc2a6f8671253cbb8b15779e68fd4cda
ELZOOZ7 <151566779+ELZOOZ7@users.noreply.github.com>
2026-02-15 02:41:43 +0200

    Update music streaming URL in config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://drive.google.com/uc?export=download&id=1LA4W5-1zO22CXwKlEk34vzkXNV8XJQOW", // Music streaming URL
+        musicUrl: "https://drive.google.com/file/d/1LA4W5-1zO22CXwKlEk34vzkXNV8XJQOW/preview", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
